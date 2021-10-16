43d6c6452763ea432f5283d40db7a25d1f881615
Alexander Popp <Alexander.Popp@Sarsy-srl.com>
2021-10-16 18:53:12 -0300

    page title from environment variables

--- packages/react-scripts/config/env.js
+++ packages/react-scripts/config/env.js
@@ -85,6 +85,7 @@ function getClientEnvironment(publicUrl) {
         // This should only be used as an escape hatch. Normally you would put
         // images into the `src` and `import` them in code to get their paths.
         PUBLIC_URL: publicUrl,
+        PUBLIC_TITLE: process.env.PUBLIC_TITLE,
         // We support configuring the sockjs pathname during development.
         // These settings let a developer run multiple simultaneous projects.
         // They are used as the connection `hostname`, `pathname` and `port`
